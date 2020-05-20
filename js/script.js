d2f28a81ae06919207104f6cdc4678fbc5e4c8b1
Luke-McShane <lukejsmcshane@gmail.com>
2020-05-20 11:16:00 +1000

    Completed aesthetic of project. Unnecessary JS file created due to attempting to manipulate hamburger menu through JS. Code needs some refactoring for organization, and comments need adding.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+// var links = document.getElementsByClassName('overlay-link');
+// var menu = document.getElementById('menu-id');
+// var textContainer = document.getElementById('text-container-id');
+// // links.onclick = function() {
+// //   // const toggle = document.getElementById('toggle'); 
+// //   // toggle.checked = true;
+// //   menu.style.scale = 1;
+// //   textContainer.style.opacity = 1;
+// //   console.log('asd');
+// // }
+
+// console.log(links);
+
+// console.log(menu);
+
+// console.log(textContainer);
+// console.log(links.length);
+// for (var i = 0; i < links.length; i++) {
+//   // console.log(links[i]);
+//   links[i].addEventListener("click", function(){
+//     // var toggle = document.getElementById('toggle'); 
+//     // toggle.checked = true;
+//     menu.style.scale = 0;
+//     textContainer.style.opacity = 0;
+//     console.log('asd');
+//   });
+// };
